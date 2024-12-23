2a2d1ed7d9829187690202ce7acfcc49f5b922fe
Steve Bauman <steven_bauman@outlook.com>
2024-12-23 10:23:04 -0500

    Update InvalidMessageDateException.php

--- src/Exceptions/InvalidMessageDateException.php
+++ src/Exceptions/InvalidMessageDateException.php
@@ -1,16 +1,5 @@
 <?php
 
-/*
-* File:     InvalidMessageDateException.php
-* Category: Exception
-* Author:   M. Goldenbaum
-* Created:  10.03.19 04:31
-* Updated:  -
-*
-* Description:
-*  -
-*/
-
 namespace Webklex\PHPIMAP\Exceptions;
 
 use Exception;
